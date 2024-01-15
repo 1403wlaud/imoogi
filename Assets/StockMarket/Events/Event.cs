d8a353ca8d2d52c594589ad4953b9bb562c68551
1403wlaud <jeemyung1129@gmail.com>
2024-01-15 20:47:24 +0900

    Event
    event

--- Assets/StockMarket/Events/Event.cs
+++ Assets/StockMarket/Events/Event.cs
@@ -1,12 +1,16 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Event", menuName = "Event/Event")]
 public class Event : ScriptableObject
 {
     [Header("Global")]
+    public DateTime startDate;
+    public DateTime endDate;
     public Sprite headerSprite;
     public string header;
     [Multiline] public string description;
 
+    public virtual void End(Game game) { }
     public virtual void Act(Game game) { }
 }
